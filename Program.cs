a62f8782db610bdae2ffc3bb7575bb20939afa1e
Myles Marcus <152407286+MylesMarcus102@users.noreply.github.com>
2026-07-31 17:51:22 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,69 +1,87 @@
-﻿public class ForceCalculator
+﻿public class UserObject
 {
-    public double G;
-    public double mass2;
     public double mass;
     public double x;
     public double y;
-    public double z;
-    public double x2;
-    public double y2;
-    public double z2;
-    public (double x, double y, double z) Position;
-    public void Introduction()
-    {
-        Console.WriteLine("This will calculate the gravitational force two objects would have on each other");
-    }
+    public string Name = "";
+    public (double x, double y) Position;
 
-    public (double,double,double,double, double) Setup()
+    public (double, double, double) Setup()
     {
-        while (true)
-        {
-            Console.WriteLine("Please type the mass of the object in kilograms");
+            Console.WriteLine($"Please type the mass of the {Name}object in kilograms");
             mass = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please type the x coordinates in meters of your object");
+            Console.WriteLine($"Please type the x coordinates in meters of your {Name}object");
             x = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please type the y coordinates in meters of your object");
+            Console.WriteLine($"Please type the y coordinates in meters of your {Name}object");
             y = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please type the z coordinates in meters of your object");
-            z = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please type the mass of the second object in kilograms");
-            mass2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please type the x coordinates in meters of your second object");
-            x2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please type the y coordinates in meters of your second object");
-            y2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please type the z coordinates in meters of your second object");
-            z2 = Convert.ToDouble(Console.ReadLine());
-            G = 6.6743 * Math.Pow(10, -11);
-            Position = (x, y, z);
-            if (x2 == x && y2 == y && z2 == z)
-            {
-                Console.WriteLine("Please retype the coordinates because they occupy the same space");
-            }
-            else
-            {
-                break;
-            }   
-            
-        }
-        return (G, mass, x, y, z);
+            Position = (x, y);
+            return (mass, x, y);
     }
-    public void Force()
+    
+    }
+public class ForceCalculator
+{
+    public void ForceMath()
     {
-            double Distance = Math.Sqrt((x-x2)*(x-x2)+(y-y2)*(y-y2)+(z-z2)*(z-z2));
-            double force = (mass2 * mass*G)/Math.Pow(Distance,2);
-            Console.WriteLine($"Your objects are pulling each other at {force} newtons.");
+        double G = 6.6743e-11;
+        UserObject Object1 = new UserObject();
+        UserObject Object2 = new UserObject();
+        Object2.Name = "second ";
+        Object1.Setup();
+        Object2.Setup();
+        while (Object1.Position == Object2.Position)
+        {
+            Console.WriteLine("Please fix your values, your objects occupy the same space");
+            Object1.Setup();
+            Object2.Setup();
+        }
+
+        double DTStep = 0;
+        double DX1;
+        double DX2;
+        double DY1;
+        double DY2;
+        double DT = 0.01;
+        double VelocityX1 = 0;
+        double VelocityX2 = 0;
+        double VelocityY1 = 0;
+        double VelocityY2 = 0;
+        Console.WriteLine("");
+        while (Object1.Position != Object2.Position)
+        {
+            double Distance = Math.Sqrt((Object1.x-Object2.x)*(Object1.x-Object2.x)+(Object1.y-Object2.y)*(Object1.y-Object2.y));
+            DY1 = (Object2.y - Object1.y);
+            DY2 = (Object1.y - Object2.y);
+            DX1 = (Object2.x - Object1.x);
+            DX2 = (Object1.x - Object2.x);
+            double force = (Object1.mass * Object2.mass*G)/Math.Pow(Distance,2);
+            double AccelerationX1 = force/Object1.mass*(DX1/Distance);
+            double AccelerationX2 = (force/Object2.mass)*(DX2/Distance);
+            double AccelerationY1 = force/Object1.mass*(DY1/Distance);
+            double AccelerationY2 = (force/Object2.mass)*(DY2/Distance);
+            Console.Write($"\r Object1({Object1.Position.x} meters,{Object1.Position.y} meters) Object2({Object2.Position.x} meters,{Object2.Position.y} meters) Time:{DTStep} seconds. Your objects are pulling each other at {force} newtons.");
+            VelocityX1 += AccelerationX1*DT;
+            VelocityX2 += AccelerationX2*DT;
+            VelocityY1 += AccelerationY1*DT;
+            VelocityY2 += AccelerationY2*DT;
+            Object1.x += VelocityX1*DT;
+            Object1.y += VelocityY1*DT;
+            Object2.x += VelocityX2*DT;
+            Object2.y += VelocityY2*DT;
+            Object1.Position = (Object1.x,Object1.y);
+            Object2.Position = (Object2.x,Object2.y);
+            DTStep += DT;
+            System.Threading.Thread.Sleep(10);
+        }
     }
 }
+    
 
-public class Program
+    public class Program
 {
     public static void Main()
     {
         ForceCalculator Force = new ForceCalculator();
-        Force.Introduction();
-        Force.Setup();
-        Force.Force();
+        Force.ForceMath();
     }
 }
